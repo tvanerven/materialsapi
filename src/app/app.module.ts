99a238ed4c877278b9eb78ab97b658b38de0c5ea
Charly <maeder@cines.fr>
2021-10-05 15:43:59 +0200

    Add sorting in result table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTableModule,
     MatPaginatorModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -1,9 +1,12 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { Component, Input, OnChanges } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, ViewChild } from '@angular/core';
 import { MATERIALS } from '../../assets/materials';
 import { Material } from '../common/material';
 import { CONCEPTS } from '../../assets/concepts';
 import { Concept } from '../common/concept';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-result-list',
@@ -11,24 +14,28 @@ import { Concept } from '../common/concept';
   styleUrls: ['./result-list.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('collapsed, void', style({ height: '0px' })),
       state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
+      transition('expanded <=> void', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))]),
   ],
 })
-export class ResultListComponent implements OnChanges {
+export class ResultListComponent implements OnChanges, AfterViewInit {
   @Input() conceptFilter: Concept[] = [];
 
-  dataSource = MATERIALS;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  dataSource = new MatTableDataSource(MATERIALS);
   columnsToDisplay = ['name', 'author'];
   expandedMaterial: Material | null | undefined;
 
   ngOnChanges() {
     if (this.conceptFilter.length > 0) {
-      this.dataSource = MATERIALS;
+      this.dataSource.data = MATERIALS;
     }
-    this.dataSource = MATERIALS.filter((material: Material) => {
+
+    this.dataSource.data = MATERIALS.filter((material: Material) => {
       for (const conceptFilter of this.conceptFilter) {
         if (!material.concept.includes(conceptFilter.rdfAbout)) {
           return false;
@@ -36,6 +43,15 @@ export class ResultListComponent implements OnChanges {
       }
       return true;
     });
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   getConcept(rdfABout: string): Concept | undefined {
