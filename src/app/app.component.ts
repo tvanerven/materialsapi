3db43cb40dad4d21fe2aba368a0e73bc3f9c29e6
Charly <maeder@cines.fr>
2021-10-28 11:54:23 +0200

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { EnvironmentService } from './services/environment.service';
 
@@ -7,7 +7,7 @@ import { EnvironmentService } from './services/environment.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @ViewChild('searchBar') searchBar!: SearchBarComponent;
 
@@ -15,8 +15,33 @@ export class AppComponent {
     private envService: EnvironmentService
   ) { }
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.showHideFooter);
+
+    const content = document.getElementById('content');
+    if (content != null) {
+      new ResizeObserver(() => this.showHideFooter()).observe(content);
+    }
+  }
+
   get apiUrl(): string {
     return this.envService.environment.apiUrl;
   }
 
+  showHideFooter(): void {
+    const body = document.body;
+    const html = document.documentElement;
+
+    const height = Math.max(body.scrollHeight, body.offsetHeight,
+      html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+    const scroll = document.body.clientHeight + window.scrollY;
+    const footer = document.getElementById('footer');
+
+    if (footer == null) {
+      return;
+    }
+
+    footer.hidden = scroll !== height;
+  }
 }
